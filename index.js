34d076a59fbdacc06f02e4aca91d816fb482cee5
mayank-jain012 <99378206+mayank-jain012@users.noreply.github.com>
2023-08-31 13:18:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+const mainImg=document.getElementById("mainImg");
+const smallImg=document.getElementsByClassName("small-img");
+smallImg[0].onclick= function(){
+    mainImg.src=smallImg[0].src;
+}
+smallImg[1].onclick= function(){
+    mainImg.src=smallImg[1].src;
+}
+smallImg[2].onclick= function(){
+    mainImg.src=smallImg[2].src;
+}
+smallImg[3].onclick= function(){
+    mainImg.src=smallImg[3].src;
+}
